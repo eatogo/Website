5170685effd2f3868b411c6e5f9ed84c87f93246
idontwannarock <idontwannarock@gmail.com>
2018-03-01 23:17:25 +0800

    Added store landing, store home, and store info page.

---
+++ static/js/common.js
@@ -0,0 +1,27 @@
+$(this).ready(function () {
+    
+});
+
+// Collapsable Navbar
+$('.ui.toggle.icon').click(function () {
+    $('.mobile-item').toggleClass('custom-mobile-hide', 1000, 'easeInOut');
+    $('.ui.toggle.icon').toggleClass('custom-collapse-navbar-button-top');
+    if ($('.ui.main.container').css('padding-top') == '120px') {
+        $('.ui.main.container').attr('style', 'padding-top: 296px !important');
+    } else {
+        $('.ui.main.container').attr('style', 'padding-top: 120px !important');
+    }
+});
+
+// TWZipcode
+$("#twzipcode").twzipcode({
+    countySel: "臺北市", // 城市預設值, 字串一定要用繁體的 "臺", 否則抓不到資料
+    districtSel: "大安區", // 地區預設值
+    zipcodeIntoDistrict: true, // 郵遞區號自動顯示在地區
+    css: ["city menu field", "menu field"], // 自訂 "城市"、"地區" class 名稱 
+    countyName: "storeCity", // 自訂城市 select 標籤的 name 值
+    districtName: "storeArea" // 自訂地區 select 標籤的 name 值
+});
+
+// Semantic UI Dropdown
+$('.ui.dropdown').dropdown();
