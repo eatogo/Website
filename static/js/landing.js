f7ae639f181d3eb2521b9b9a2108beb476a23b4b
idontwannarock <idontwannarock@gmail.com>
2018-03-20 18:09:48 +0800

    Finished store info function.

--- static/js/landing.js
+++ static/js/landing.js
@@ -103,7 +103,7 @@ function setStoresDiv() {
             extractPayloadToStoresDiv();
             setStoreLinkClickEventListener();
         }
-    })
+    {}})
 }
 
 function extractPayloadToStoresDiv() {
